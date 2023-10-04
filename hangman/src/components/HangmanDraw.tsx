8a2b4897b52a1e40d61d9d80434fab01ca91c7df
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-10-04 16:59:23 +0530

    hangman drawing

--- hangman/src/components/HangmanDraw.tsx
+++ hangman/src/components/HangmanDraw.tsx
@@ -2,13 +2,44 @@ import React from 'react'
 import styled from 'styled-components'
 
 
+const HEAD = (
+    <div style={{width:"50px", height:"50px", borderRadius:"100%", border:"10px solid black", position:"absolute", top:"50px", right:"-30px"}}/>
+)
+
+const BODY = (
+    <div style={{width:"10px", height:"120px", background:"black", position:"absolute", top:"120px", right:"0px"}}/>
+)
+
+const RIGHT_HAND = (
+    <div style={{width:"100px", height:"10px", background:"black", position:"absolute", top:"150px", right:"-100px", rotate:"-30deg", transformOrigin:"left bottom"}}/>
+)
+
+const LEFT_HAND = (
+    <div style={{width:"100px", height:"10px", background:"black", position:"absolute", top:"150px", right:"10px", rotate:"30deg", transformOrigin:"right bottom"}}/>
+)
+
+const RIGHT_LEG = (
+    <div style={{width:"120px", height:"10px", background:"black", position:"absolute", top:"230px", right:"-110px", rotate:"60deg", transformOrigin:"left bottom"}}/>
+)
+
+const LEFT_LEG = (
+    <div style={{width:"120px", height:"10px", background:"black", position:"absolute", top:"230px", right:"0px", rotate:"-60deg", transformOrigin:"right bottom"}}/>
+)
+
+
 const HangmanDraw = () => {
   return (
     <DIV>
+       {HEAD}
+       {BODY}
+       {RIGHT_HAND}
+       {LEFT_HAND}
+       {RIGHT_LEG}
+       {LEFT_LEG}
         <DIV_small/>
         <DIVHor/>
         <DIVVer/>
-            <DIVB/>
+        <DIVB/>
     </DIV>
   )
 }
@@ -42,5 +73,4 @@ const DIV_small=styled.div`
     position: absolute;
     right: 0px;
     top: 0px;
-    
 `
